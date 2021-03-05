a97719b8521d7ed59565cdba3a19be4ae480aec5
azha <anatole.zha@gmail.com>
2021-03-05 12:39:26 +0100

    Add getter and test for FileFormatter

--- src/main/java/FileFormatter.java
+++ src/main/java/FileFormatter.java
@@ -6,27 +6,26 @@ import java.util.ArrayList;
 public class FileFormatter {
     private ArrayList<String> linesOfFile;
 
-    public FileFormatter(String pathToEntryFile) {
+    public FileFormatter(String pathToEntryFile) throws IOException {
         this.linesOfFile = fileToStringArrayListByLines(pathToEntryFile);
     }
 
-    public FileFormatter() { }
-
-    public static ArrayList<String> fileToStringArrayListByLines(String filePath){
+    public static ArrayList<String> fileToStringArrayListByLines(String filePath) throws IOException {
         ArrayList<String> lines = new ArrayList<>();
         BufferedReader fileReader;
 
-        try {
-            fileReader = new BufferedReader(new FileReader(filePath));
-            String line = fileReader.readLine();
-            while (line != null){
-                lines.add(line);
-                line = fileReader.readLine();
-            }
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        fileReader = new BufferedReader(new FileReader(filePath));
+        String line = fileReader.readLine();
+        while (line != null){
+            lines.add(line);
+            line = fileReader.readLine();
         }
+        fileReader.close();
+
         return lines;
+    }
+
+    public ArrayList<String> getLinesOfFile() {
+        return linesOfFile;
     }
 }
--- src/test/java/FileFormatterTest.java
+++ src/test/java/FileFormatterTest.java
@@ -1,17 +1,18 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.BufferedReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FileFormatterTest {
 
@@ -24,22 +25,22 @@ public class FileFormatterTest {
     }
 
     @Test
-    public void should_return_ArrayList_with_each_line_of_file_in_it(){
-        Path path  = tempDir.resolve("testFile.txt");
+    public void should_return_ArrayList_with_each_line_of_file_in_it() throws IOException {
+        Path path = tempDir.resolve("testFile.txt");
 
         try {
             FileWriter fileWriter = new FileWriter(path.toString());
             fileWriter.write(
-                    "C - 3 - 4\n"+
-                    "M - 1 - 1\n"+
-                    "M - 2 - 2\n"+
-                    "T - 0 - 3 - 2\n"+
-                    "T - 1 - 3 - 1");
+                    "C - 3 - 4\n" +
+                            "M - 1 - 1\n" +
+                            "M - 2 - 2\n" +
+                            "T - 0 - 3 - 2\n" +
+                            "T - 1 - 3 - 1");
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ArrayList<String> expectedResult = new ArrayList<>(){{
+        ArrayList<String> expectedResult = new ArrayList<>() {{
             add("C - 3 - 4");
             add("M - 1 - 1");
             add("M - 2 - 2");
@@ -47,5 +48,38 @@ public class FileFormatterTest {
             add("T - 1 - 3 - 1");
         }};
         assertEquals(expectedResult, FileFormatter.fileToStringArrayListByLines(path.toString()));
+    }
+
+    @Test
+    public void should_throw_FileNotFoundException_if_file_doesnt_exist() {
+        String fakePath = "/this/path/dont/work/file.txt";
+        assertThrows(FileNotFoundException.class, () -> FileFormatter.fileToStringArrayListByLines(fakePath));
+    }
+
+    @Test
+    public void should_initiate_linesOfFile_with_each_line_of_file_in_it() throws IOException {
+        Path path = tempDir.resolve("testFile.txt");
+
+        try {
+            FileWriter fileWriter = new FileWriter(path.toString());
+            fileWriter.write(
+                    "C - 3 - 4\n" +
+                            "M - 1 - 1\n" +
+                            "M - 2 - 2\n" +
+                            "T - 0 - 3 - 2\n" +
+                            "T - 1 - 3 - 1");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> expectedResult = new ArrayList<>() {{
+            add("C - 3 - 4");
+            add("M - 1 - 1");
+            add("M - 2 - 2");
+            add("T - 0 - 3 - 2");
+            add("T - 1 - 3 - 1");
+        }};
+        FileFormatter fileFormatter = new FileFormatter(path.toString());
+        assertEquals(expectedResult, fileFormatter.getLinesOfFile());
     }
 }
